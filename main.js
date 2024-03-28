f2596f6efc1fe23c06fae83eea19921c79ad81f1
Ghulam Muneer uddin <gmugsk@gmail.com>
2024-03-28 11:00:14 +0500

    Assignment 07 A

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-"use strict";
 /*Number Eight: Write addition, subtraction, multiplication,
 and division operations that each result in the number 8.
  Be sure to enclose your operations in print statements to see
   the results.*/
-const addition = 2 + 9;
-const subtraction = 4 - 3;
-const multiplication = 5 * 2;
-const division = 50 / 5;
+var addition = 1 + 7;
+var subtraction = 9 - 1;
+var multiplication = 4 * 2;
+var division = 40 / 5;
 console.log(addition, subtraction, multiplication, division);
--- main.ts
+++ main.ts
@@ -3,10 +3,10 @@ and division operations that each result in the number 8.
  Be sure to enclose your operations in print statements to see
   the results.*/
 
-  const addition: number = 2 + 9;
-  const subtraction: number = 4-3;
-  const multiplication: number = 5*2;
-  const division: number = 50/5;
+  const addition: number = 1 + 7;
+  const subtraction: number =9-1;
+  const multiplication: number = 4*2;
+  const division: number = 40/5;
 
   console.log(addition, subtraction,multiplication,division);
 
